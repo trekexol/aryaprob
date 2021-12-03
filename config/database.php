a43e4aa8dcdad07438e6bb7fae9ab9d1030fba2a
trekexol <cefreitas.16@gmail.com>
2021-12-03 09:53:45 -0400

    gianestballon

--- config/database.php
+++ config/database.php
@@ -205,6 +205,25 @@ return [
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
             ]) : [],
         ],
+        'gianestballoon' => [
+            'driver' => 'mysql',
+            'url' => env('DATABASE_URL'),
+            'host' => env('GIANESTBALLOON_HOST', '127.0.0.1'),
+            'port' => env('GIANESTBALLOON_PORT', '3306'),
+            'database' => env('GIANESTBALLOON_DATABASE', 'forge'),
+            'username' => env('GIANESTBALLOON_USERNAME', 'forge'),
+            'password' => env('GIANESTBALLOON_PASSWORD', ''),
+            'unix_socket' => env('GIANESTBALLOON_SOCKET', ''),
+            'charset' => 'utf8mb4',
+            'collation' => 'utf8mb4_unicode_ci',
+            'prefix' => '',
+            'prefix_indexes' => true,
+            'strict' => true,
+            'engine' => null,
+            'options' => extension_loaded('pdo_mysql') ? array_filter([
+                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+            ]) : [],
+        ],
 
         'pgsql' => [
             'driver' => 'pgsql',
