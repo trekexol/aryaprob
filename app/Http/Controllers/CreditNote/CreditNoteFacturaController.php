ad181e2124b25a6569a2ae7a9997e0da74d31bba
trekexol <cefreitas.16@gmail.com>
2021-12-06 10:38:38 -0400

    asd

---
+++ app/Http/Controllers/CreditNote/CreditNoteFacturaController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\CreditNote;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CreditNoteFacturaController extends Controller
+{
+    //
+}
