0699a845ddf60ec00e18179a6736eb631923c6c2
trekexol <cefreitas.16@gmail.com>
2021-12-15 15:07:11 -0400

    final

--- app/Http/Controllers/AccountingAdjustmentController.php
+++ app/Http/Controllers/AccountingAdjustmentController.php
@@ -2,9 +2,88 @@
 
 namespace App\Http\Controllers;
 
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+
+use App;
+use App\Account;
+use App\Http\Controllers\UserAccess\UserAccessController;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Auth;
 
 class AccountingAdjustmentController extends Controller
 {
-    //
+    public $userAccess;
+    public $modulo = 'Cotizacion';
+
+    public function __construct(){
+
+        $this->middleware('auth');
+        $this->userAccess = new UserAccessController();
+    }
+ 
+    public function index($coin = null)
+    {
+        if($this->userAccess->validate_user_access($this->modulo)){
+         
+            $date = Carbon::now();
+            $datenow = $date->format('Y-m-d');
+            if(empty($coin)){
+                $coin = "bolivares";
+            }
+
+            $detailvouchers = DB::connection(Auth::user()->database_name)->table('detail_vouchers')
+                            ->join('header_vouchers', 'header_vouchers.id', '=', 'detail_vouchers.id_header_voucher')
+                            ->join('accounts', 'accounts.id', '=', 'detail_vouchers.id_account')
+                            ->where('header_vouchers.date', $datenow)
+                            ->select('detail_vouchers.*','header_vouchers.*'
+                            ,'accounts.description as account_description')->get();
+            
+            $accounts = Account::on(Auth::user()->database_name)->select('id','description')->where('code_one','<>',0)
+                            ->where('code_two','<>',0)
+                            ->where('code_three','<>',0)
+                            ->where('code_four','<>',0)
+                            ->where('code_five', '<>',0)
+                            ->get();
+            
+
+
+            return view('admin.accounting_adjustments.index',compact('detailvouchers','datenow','accounts','coin'));
+
+        }else{
+            return redirect('/home')->withDanger('No tiene Acceso al modulo de '.$this->modulo);
+        }
+        
+ 
+        
+    }
+
+    public function store(Request $request)
+    {
+        $data = request()->validate([
+            'date_begin'        =>'required',
+            'date_end'          =>'required',
+        ]);
+        
+        $date_begin = request('date_begin');
+        $date_end = request('date_end');
+
+        $detailvouchers =  DB::connection(Auth::user()->database_name)->table('detail_vouchers')
+                                ->join('header_vouchers', 'header_vouchers.id', '=', 'detail_vouchers.id_header_voucher')
+                                ->join('accounts', 'accounts.id', '=', 'detail_vouchers.id_account')
+                                ->whereBetween('header_vouchers.date', [$date_begin, $date_end])
+                                ->select('detail_vouchers.*','header_vouchers.*'
+                                ,'accounts.description as account_description')
+                                ->orderBy('detail_vouchers.id','desc')->get();
+
+        $accounts = Account::on(Auth::user()->database_name)->select('id','description')->where('code_one','<>',0)
+                                ->where('code_two','<>',0)
+                                ->where('code_three','<>',0)
+                                ->where('code_four','<>',0)
+                                ->where('code_five', '<>',0)
+                                ->get();
+                                
+        return view('admin.accounting_adjustments.index',compact('detailvouchers','date_begin','date_end','accounts'));
+   
+    }
 }
