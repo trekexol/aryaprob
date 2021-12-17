4e5c6d573085075aa7aa465971427b644627da64
trekexol <cefreitas.16@gmail.com>
2021-12-17 11:01:52 -0400

    sdf

--- routes/web.php
+++ routes/web.php
@@ -842,11 +842,11 @@ Route::group(["prefix"=>'reports'],function(){
     Route::post('storeshopping','Reports\ShoppingController@store_shopping')->name('reports.store_shopping');
     Route::get('shoppingpdf/{coin}/{date_begin}/{date_end}/{name?}','Reports\ShoppingController@shopping_pdf')->name('reports.shopping_pdf');
     
-    /*
+    
     Route::get('accounts_receivable_note/{typeperson}/{id_client?}','Report2Controller@index_accounts_receivable_note')->name('reports.accounts_receivable_note'); // dacson (report note delivery)
     Route::post('storeaccounts_receivable_note','Report2Controller@store_accounts_receivable_note')->name('reports.store_accounts_receivable_note'); // dacson (report note delivery)
     Route::get('accounts_receivable_note_pdf/{coin}/{date_end?}/{date_begin?}/{typeinvoice}/{typeperson}','Report2Controller@accounts_receivable_note_pdf')->name('reports.accounts_receivable_note_pdf');
-*/
+
 });
 
 
